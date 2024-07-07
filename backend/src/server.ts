45c86122d474b22321eded1a64cef6e16d193ecc
toufiq007 <t.limon2342@gmail.com>
2024-07-07 09:57:00 +0600

    setup cloudinary

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,12 @@
 import app from "./app";
+import cloudinary from "cloudinary";
 const port = process.env.PORT;
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(port, () => {
   console.log(
